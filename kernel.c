1380a2d8de245296045d47165926d908c1845865
matveeu <subbotinmatvey008@mail.ru>
2026-07-12 18:33:50 +0300

    v1.0.3: Added some commands: help, clear, reboot

--- kernel.c
+++ kernel.c
@@ -2,9 +2,11 @@ char* video = (char*) 0xB8000;
 int cursor_x = 0;
 int cursor_y = 0;
 
-char cmd_buffer[64];
+#define MAX_CMD 128
+char cmd_buffer[MAX_CMD];
 int cmd_index = 0;
 
+// Очистка экрана
 void clearScreen() {
         for (unsigned int i = 0; i < 80 * 25 * 2; i += 2) {
                 video[i] = ' ';
@@ -20,10 +22,22 @@ unsigned char inb(unsigned short port) {
         return result;
 }
 
+void outb(unsigned short port, unsigned char data) {
+        __asm__ volatile ("outb %0, %1" : : "a"(data), "Nd"(port));
+}
+
+// Размещение символа в видеопамять
 void putchar(char c) {
         if (c == '\n') {
                 cursor_x = 0;
                 cursor_y++;
+        } else if (c == '\b') {
+                if (cursor_x > 0) {
+                        cursor_x--;
+                        int pos = (cursor_y * 80 + cursor_x) *2;
+                        video[pos] = ' ';
+                        video[pos + 1] = 0x0F;
+                }
         } else {
                 int pos = (cursor_y * 80 + cursor_x) * 2;
                 video[pos] = c;
@@ -46,12 +60,14 @@ void putchar(char c) {
         }
 }
 
+// Печать строки
 void print(const char* str) {
         for (unsigned int i = 0; str[i] != '\0'; i++) {
                 putchar(str[i]);
         }
 }
 
+// Таблица символов
 char keymap[] = {
         0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0, 0,
         'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0, 0,
@@ -59,6 +75,7 @@ char keymap[] = {
         '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' '
 };
 
+// Получение символа с клавиатуры
 char getchar() {
         unsigned char scancode;
         while (1) {
@@ -75,10 +92,37 @@ char getchar() {
         }
 }
 
+// ===== КОМАНДЫ =====
+
+// help
+void show_help() {
+        print("Available commands:\n");
+        print("  help     - show this message\n");
+        print("  clear    - clear screen\n");
+        print("  reboot   - restart system\n");
+}
+
+// Сами команды
+void execute_command() {
+        if (cmd_buffer[0] == 'h' && cmd_buffer[1] == 'e' && cmd_buffer[2] == 'l' && cmd_buffer[3] == 'p') {
+                show_help();
+        } else if (cmd_buffer[0] == 'c' && cmd_buffer[1] == 'l' && cmd_buffer[2] == 'e' && cmd_buffer[3] == 'a' && cmd_buffer[4] == 'r') {
+                clearScreen();
+        } else if (cmd_buffer[0] == 'r' && cmd_buffer[1] == 'e' && cmd_buffer[2] == 'b' && cmd_buffer[3] == 'o' && cmd_buffer[4] == 'o' && cmd_buffer[5] == 't') {
+                print("Rebooting...\n");
+                outb(0x64, 0xFE); 
+                while(1);
+        } else if (cmd_buffer[0] != '\0') {
+                print("Unknown command: ");
+                print(cmd_buffer);
+                putchar('\n');
+        }
+}
+
 void kmain() {
         clearScreen();
-        print("TetoOS v1.0.1\n");
-        print("Type something...\n");
+        print("TetoOS v1.0.3\n");
+        print("Type 'help' for commands.\n");
         print("> ");
 
         while(1) {
@@ -87,21 +131,22 @@ void kmain() {
                 if (ch == '\n') {
                         putchar('\n');
                         cmd_buffer[cmd_index] = '\0';
+                        execute_command();
                         print("> ");
 
                         cmd_index = 0;
-                        for (unsigned int i = 0; i < 64; i++) cmd_buffer[i] = '\0';
+                        for (unsigned int i = 0; i < MAX_CMD; i++) cmd_buffer[i] = '\0';
                 }
                 else if (ch == '\b') {
                         if (cmd_index > 0) {
                                 cmd_index--;
                                 putchar('\b');
-                                putchar(' ');
-                                putchar('\b');
+                                // putchar(' ');
+                                // putchar('\b');
                         }
                 }
                 else {
-                        if (cmd_index < 64 - 1) {
+                        if (cmd_index < MAX_CMD - 1) {
                                 cmd_buffer[cmd_index++] = ch;
                                 putchar(ch);
                         }
