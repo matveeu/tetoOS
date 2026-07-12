1f423cf65ab1fe3843c2a3fb657a487458044087
matveeu <subbotinmatvey008@mail.ru>
2026-07-12 22:02:19 +0300

    v1.0.5: Some changes, added README.md, .gitignore and ISO file for VirtualBox.

--- kernel.c
+++ kernel.c
@@ -1,8 +1,9 @@
+#include <stddef.h>
 char* video = (char*) 0xB8000;
 int cursor_x = 0;
 int cursor_y = 0;
 
-#define MAX_CMD 128
+#define MAX_CMD 256
 char cmd_buffer[MAX_CMD];
 int cmd_index = 0;
 
@@ -76,6 +77,50 @@ void putchar(char c) {
                 update_cursor();
         }
 }
+// История команд (на стадии разработки)
+/*#define HISTORY_SIZE 8
+char history[HISTORY_SIZE][MAX_CMD];
+int history_count = 0;
+int history_pos = -1;
+
+void add_history(const char* cmd) {
+        if (cmd[0] == '\0') return;
+        for (int i = HISTORY_SIZE - 1; i > 0; i--) {
+                for (int j = 0; j < MAX_CMD; j++) {
+                        history[i][j] = history[i-1][j];
+                }
+        }
+
+        for (int i = 0; i < MAX_CMD && cmd[i] != '\0'; i++) {
+                history[0][i] = cmd[i];
+                history[0][i+1] = '\0';
+        }
+        if (history_count < HISTORY_SIZE) history_count++;
+        history_pos = -1;
+}
+
+void load_history(int offset) {
+        int new_pos = history_pos + offset;
+        if (new_pos < -1 || new_pos >= history_count) return;
+        history_pos = new_pos;
+
+        while (cmd_index > 0) {
+                putchar('\b');
+                cmd_index--;
+        }
+
+        if (history_pos == -1) {
+                for (int i = 0; i < MAX_CMD; i++) cmd_buffer[i] = '\0';
+                return;
+        }
+
+        for (int i = 0; i < MAX_CMD; i ++) {
+                cmd_buffer[i] = history[history_pos][i];
+                if (cmd_buffer[i] == '\0') break;
+                putchar(cmd_buffer[i]);
+                cmd_index = i+1;
+        }
+}*/
 
 // Печать строки
 void print(const char* str) {
@@ -98,6 +143,16 @@ char getchar() {
         while (1) {
                 while ((inb(0x64) & 1) == 0);
                 scancode = inb(0x60);
+
+                if (scancode >= 0x80) continue;
+                if (scancode == 0xE0) {
+                                while ((inb(0x64) & 1) == 0);
+                                scancode = inb(0x60);
+                                if (scancode >= 0x80) continue;
+                                if (scancode == 0x48) return 0x80;
+                                if (scancode == 0x50) return 0x81;
+                                continue;
+                } 
                 if (scancode < 0x80) {
                         if (scancode == 0x1C) return '\n';
                         if (scancode == 0x0E) return '\b';
@@ -133,6 +188,7 @@ void execute_command() {
         } else if (cmd_buffer[0] == 'p' && cmd_buffer[1] == 'o' && cmd_buffer[2] == 'w' && cmd_buffer[3] == 'e' && cmd_buffer[4] == 'r' && cmd_buffer[5] == 'o' && cmd_buffer[6] == 'f' && cmd_buffer[7] == 'f') {
                 print("Shutting down...\n");
                 outw(0x604, 0x2000);
+                outw(0x4004, 0x3400);
                 while(1);
         } else if (cmd_buffer[0] != '\0') {
                 print("Unknown command: ");
@@ -143,7 +199,7 @@ void execute_command() {
 
 void kmain() {
         clearScreen();
-        print("TetoOS v1.0.4\n");
+        print("TetoOS v1.0.5\n");
         print("Type 'help' for commands.\n");
         print("> ");
 
@@ -163,15 +219,21 @@ void kmain() {
                         if (cmd_index > 0) {
                                 cmd_index--;
                                 putchar('\b');
-                                // putchar(' ');
-                                // putchar('\b');
                         }
+                } /*
+                else if (ch == 0x80) {
+                        load_history(-1);
                 }
+                else if (ch == 0x81) {
+                        load_history(1);
+                } */ 
                 else {
                         if (cmd_index < MAX_CMD - 1) {
                                 cmd_buffer[cmd_index++] = ch;
                                 putchar(ch);
                         }
                 }
+                //putchar(ch); // Для отладки
         }
+
 }
